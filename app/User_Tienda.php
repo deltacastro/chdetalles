5177fc8061620ef5996441355504f1804c58e05b
deltacastro <abel.castro9111@gmail.com>
2020-02-08 18:43:33 +0000

    Se actualiza

--- app/User_Tienda.php
+++ app/User_Tienda.php
@@ -8,4 +8,9 @@ class User_Tienda extends Model
 {
     protected $table = 'user_store';
     protected $primarykey = 'iduser_store';
+
+    public function store()
+    {
+        return $this->belongsTo(Tienda::class, 'idstore', 'idstore');
+    }
 }
