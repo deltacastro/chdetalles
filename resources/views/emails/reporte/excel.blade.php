50826582bd1b725d221cb8f17462b2e962ab77ef
deltacastro <abel.castro9111@gmail.com>
2020-03-04 00:08:51 +0000

    Se agregan vistas emails

---
+++ resources/views/emails/reporte/excel.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Reporte del mes
+
+Se envia reporte automatico del mes.
+
+@component('mail::button', ['url' => ''])
+Descargar
+@endcomponent
+
+Attentamente,<br>
+Chedraui a Domicilio
+@endcomponent
